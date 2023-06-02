9ad57dfed3a85044c8c26fc6b60fa00af5412bbc
sy-k-98 <sb05220@naver.com>
2023-06-02 13:19:40 +0900

    feat : sqlite에 데이터 저장

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.models.Customer;
+import com.example.demo.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -16,6 +24,18 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello world";
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return args -> {
+			repository.save(new Customer("leo", "aa"));
+
+			for (Customer customer: repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+		};
 	}
 
 }
